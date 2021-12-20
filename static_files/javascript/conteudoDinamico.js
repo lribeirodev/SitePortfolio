a1e3babdb3d4ff08723023fee42aff66fefd7c85
Lucas Ribeiro <lucasnunesr@live.com>
2021-12-19 22:51:35 -0300

    Criação do Portfolio

--- static_files/javascript/conteudoDinamico.js
+++ static_files/javascript/conteudoDinamico.js
@@ -19,10 +19,49 @@ for(let i = 0 ; i < tech_imgs.length ; i++){
 
 }
 
+function inserirConteudo(){
 
-function inserirPosts(){
-    
+var conteudo = document.getElementsByClassName("conteudo")[0];
+
+var xml = new XMLHttpRequest();
+xml.onload = function(){
+
+    items = xml.responseXML.getElementsByTagName("item");
+    for(let i = 0 ; i < items.length ; i++){
+        item = items[i];
+        var div = document.createElement('div');
+        var img = document.createElement('img');
+        var ul = document.createElement('ul');
+        var li_titulo = document.createElement('li');
+        var li_descricao = document.createElement('li');
+        var li_link = document.createElement('li');
+        var a_link = document.createElement('a');
+        var li_tecnologias = document.createElement('li');
+
+        div.className = "conteudo_item";
+        img.src = item.getElementsByTagName('imagem')[0].childNodes[0].nodeValue;
+        li_titulo.innerText = item.getElementsByTagName('titulo')[0].childNodes[0].nodeValue;
+        li_descricao.innerText = item.getElementsByTagName('descricao')[0].childNodes[0].nodeValue;
+        a_link.href = item.getElementsByTagName('link')[0].childNodes[0].nodeValue;
+        a_link.text = "Link do Projeto";
+        li_link.appendChild(a_link);
+        li_tecnologias.innerText = item.getElementsByTagName('tecnologias')[0].childNodes[0].nodeValue;
+
+        div.appendChild(img);
+        ul.appendChild(li_titulo);
+        ul.appendChild(li_descricao);
+        ul.appendChild(li_tecnologias);
+        ul.appendChild(li_link);
+        div.appendChild(ul);
+
+        conteudo.appendChild(div);
+    }
+}
+xml.open("GET","/static_files/xml/repo.xml",true);
+xml.send();
+
 }
 
 
 inserirTecnologias();
+inserirConteudo();
